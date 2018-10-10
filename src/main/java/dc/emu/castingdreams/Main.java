4abaca77e868e6fef50554d725d6f6b5f9cc4d3c
georgemoralis <giorgosmrls@gmail.com>
2018-10-10 12:42:27 +0300

    intial project with lwjgl3 depedencies

---
+++ src/main/java/dc/emu/castingdreams/Main.java
@@ -0,0 +1,12 @@
+package dc.emu.castingdreams;
+
+/**
+ *
+ * @author shadow
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello dc");
+    }
+}
